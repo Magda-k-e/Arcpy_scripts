d0d8c3d68b9055e0ca02b784ddf75368b230a0cf
unknown <magda.kentr@gmail.com>
2025-01-21 12:46:02 +0200

    get mean height

--- time_of_concentration.py
+++ time_of_concentration.py
@@ -1,3 +1,15 @@
 import math
 
 import arcpy
+
+arcpy.env.workspace = r"C:\Files\arcpy_project"
+
+# Get mean height value from DEM
+
+dem_path = "basin_dem.tif"
+mean_height_result = arcpy.GetRasterProperties_management(dem_path, "MEAN")
+mean_height = float(mean_height_result.getOutput(0))
+print("Mean height: ", mean_height)
+
+
+
